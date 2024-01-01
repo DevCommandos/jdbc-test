14d84beccedb1dba82a566d01175eada8765a94a
devCommandos <@ekrcu4814>
2024-01-01 22:43:16 +0900

    git init

---
+++ src/test/java/commandos/jdbc/JdbcTestApplicationTests.java
@@ -0,0 +1,13 @@
+package commandos.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
