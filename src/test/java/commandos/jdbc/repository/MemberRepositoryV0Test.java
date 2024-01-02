fd492ba6c5fed307cf073cad858f34b4f2c06217
devCommandos <@ekrcu4814>
2024-01-02 23:23:33 +0900

    조회 테스트

--- src/test/java/commandos/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/commandos/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,10 +1,14 @@
 package commandos.jdbc.repository;
 
 import commandos.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
@@ -14,5 +18,9 @@ class MemberRepositoryV0Test {
         //save
         Member member = new Member("memberV0", 10000);
         repository.save(member);
+
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        assertThat(findMember).isEqualTo(member);
     }
 }
