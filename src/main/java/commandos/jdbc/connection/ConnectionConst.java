e16faef92087d184cc049c55adf7aa101ec60b8e
devCommandos <@ekrcu4814>
2024-01-02 22:53:44 +0900

    JDBC API 로 H2 연동 및 멤버 추가 테스트

---
+++ src/main/java/commandos/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,7 @@
+package commandos.jdbc.connection;
+
+public abstract class ConnectionConst {
+    public static final String URL = "jdbc:h2:tcp://localhost/~/test";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "";
+}
